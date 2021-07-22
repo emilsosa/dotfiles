76b3e47ec9298984640a28d24d26e4970fcc1ee6
Emil Sosa <emiil.sosa@gmail.com>
2021-07-22 14:59:01 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh
+
+echo "Defining symlinks..."
+ln -sv "$PWD/.gitconfig" ~/.gitconfig > /dev/null 2>&1
+ln -sv "$PWD/.antigenrc" ~/.antigenrc > /dev/null 2>&1
+ln -sv "$PWD/.gitconfig" ~/.gitconfig > /dev/null 2>&1
+ln -sv "$PWD/.p10k.zsh" ~/.p10k.zsh > /dev/null 2>&1
+ln -sv "$PWD/.zshrc" ~/.zshrc > /dev/null 2>&1
+
+echo "Setup completed."
