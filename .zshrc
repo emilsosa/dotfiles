8fab792f8f4829e4fe1f3bbe182012c9c938a9d5
Emil Sosa <emiil.sosa@gmail.com>
2023-05-21 19:04:38 -0400

    Add sdkman installation.

--- .zshrc
+++ .zshrc
@@ -14,17 +14,10 @@ export LANG=en_US.UTF-8
 
 alias cls='printf "\033c"'
 
-export ANDROID_HOME=$HOME/Android/Sdk
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools
-export REACT_EDITOR=code
-
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-
 if [ -f ~/.environment ]; then
 	source ~/.environment
 fi
+
+#THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,11 +5,25 @@ if [ "$(id -u)" -ne 0 ]
   exit
 fi
 
-apt update
-apt install -y git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev
+sudo apt update
+sudo apt install -y zsh git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
 
-echo "Installing rbenv..."
+echo "Setting zsh as default shell."
+chsh -s $(which zsh)
+echo "Finished setting zsh as default shell."
+
+echo "Install oh-my-zsh."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Finished installing oh-my-zsh."
+
+echo "Installing rbenv."
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv > /dev/null 2>&1
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build > /dev/null 2>&1
 echo "gem: --no-document" > ~/.gemrc
-echo "Finished installing rbenv..."
+echo "Finished installing rbenv."
+
+echo "Installing sdkman."
+curl -s "https://get.sdkman.io" | bash
+echo "Finished installing sdkman."
+
+echo "Restart current session to apply changes."
