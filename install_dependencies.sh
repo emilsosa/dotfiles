126c18512c8393752670130b351adfb093ef50a1
Emil Sosa <emiil.sosa@gmail.com>
2023-05-21 21:28:43 -0400

    Adding .tmux.conf and tmux installation.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,14 +6,18 @@ if [ "$(id -u)" -ne 0 ]
 fi
 
 sudo apt update
-sudo apt install -y zsh git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
+sudo apt install -y zsh tmux git autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev
 
 echo "Setting zsh as default shell."
 chsh -s $(which zsh)
 echo "Finished setting zsh as default shell."
 
+echo "Install tmux plugin manager."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm > /dev/null 2>&1
+echo "Finished installing tmux plugin manager."
+
 echo "Install oh-my-zsh."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 echo "Finished installing oh-my-zsh."
 
 echo "Installing rbenv."
@@ -23,7 +27,7 @@ echo "gem: --no-document" > ~/.gemrc
 echo "Finished installing rbenv."
 
 echo "Installing sdkman."
-curl -s "https://get.sdkman.io" | bash
+curl -s "https://get.sdkman.io" | bash > /dev/null 2>&1
 echo "Finished installing sdkman."
 
 echo "Restart current session to apply changes."
--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ ln -sv "$PWD/.antigenrc" ~/.antigenrc > /dev/null 2>&1
 ln -sv "$PWD/.gitconfig" ~/.gitconfig > /dev/null 2>&1
 ln -sv "$PWD/.p10k.zsh" ~/.p10k.zsh > /dev/null 2>&1
 ln -sv "$PWD/.zshrc" ~/.zshrc > /dev/null 2>&1
+ln -sv "$PWD/.tmux.conf" ~/.tmux.conf > /dev/null 2>&1
 
 echo "Setup completed."
